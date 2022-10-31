e5e007ee733b7c437caf2c841ef65edc42f1ce1a
Sansang Studio <studiosansang@gmail.com>
2022-10-31 11:42:47 +0700

    [Fix] Add Statement Data Duplicate

--- RecordSystem/App.Program/Application/Save/FrmStatements.cs
+++ RecordSystem/App.Program/Application/Save/FrmStatements.cs
@@ -249,12 +249,12 @@ namespace SANSANG
                 if (Function.GetComboId(cbbMoney) != "0" && Function.GetComboId(cbbAccount) != "0" && Function.GetComboId(cbbTransactions) != "0" && !string.IsNullOrEmpty(txtAmount.Text))
                 {
                     if (!Function.IsDuplicates(Table.Statments,
-                        cbbMoney.Text,
+                        "ONE",
                         Function.GetComboId(cbbAccount),
                         Function.GetComboId(cbbTransactions),
                         Date.GetDate(dtp: dtDate, Format: 4),
                         Function.RemoveComma(txtAmount.Text),
-                        Function.RemoveComma("0.00")))
+                        Date.GetDate(dtp: dtDate, Format: 6) + System.Environment.NewLine + cbbTransactions.Text + " - " + txtAmount.Text))
                     {
                         txtCode.Text = Function.GetCodes(Table.StatmentId, "", "Generated");
 
