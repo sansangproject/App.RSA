886879e7aa276ce0beea056e6c67c106c18acd65
Tuathor.st <sarawut@ctc-g.co.th>
2023-04-14 19:20:38 +0700

    Update Calculate Gold Receive

--- RecordSystem/App.Program/Application/Save/FrmGolds.Designer.cs
+++ RecordSystem/App.Program/Application/Save/FrmGolds.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmGolds));
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.cbbMember = new System.Windows.Forms.ComboBox();
             this.gbForm = new System.Windows.Forms.GroupBox();
             this.label29 = new System.Windows.Forms.Label();
@@ -205,7 +205,7 @@
             // 
             this.label31.AutoSize = true;
             this.label31.Font = new System.Drawing.Font("Mitr Light", 8F);
-            this.label31.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label31.ForeColor = System.Drawing.Color.Chocolate;
             this.label31.Location = new System.Drawing.Point(493, 325);
             this.label31.Name = "label31";
             this.label31.Size = new System.Drawing.Size(51, 18);
@@ -239,7 +239,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Mitr Light", 8F);
-            this.label8.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label8.ForeColor = System.Drawing.Color.Chocolate;
             this.label8.Location = new System.Drawing.Point(482, 282);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(62, 18);
@@ -273,7 +273,7 @@
             // 
             this.label30.AutoSize = true;
             this.label30.Font = new System.Drawing.Font("Mitr Light", 8F);
-            this.label30.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label30.ForeColor = System.Drawing.Color.Chocolate;
             this.label30.Location = new System.Drawing.Point(851, 282);
             this.label30.Name = "label30";
             this.label30.Size = new System.Drawing.Size(61, 18);
@@ -309,7 +309,7 @@
             // 
             this.label16.AutoSize = true;
             this.label16.Font = new System.Drawing.Font("Mitr Light", 8F);
-            this.label16.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label16.ForeColor = System.Drawing.Color.Chocolate;
             this.label16.Location = new System.Drawing.Point(491, 201);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(53, 18);
@@ -344,7 +344,7 @@
             // 
             this.label14.AutoSize = true;
             this.label14.Font = new System.Drawing.Font("Mitr Light", 8F);
-            this.label14.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label14.ForeColor = System.Drawing.Color.Chocolate;
             this.label14.Location = new System.Drawing.Point(483, 241);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(61, 18);
@@ -379,7 +379,7 @@
             // 
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Mitr Light", 8F);
-            this.label12.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label12.ForeColor = System.Drawing.Color.Chocolate;
             this.label12.Location = new System.Drawing.Point(867, 201);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(45, 18);
@@ -414,7 +414,7 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Mitr Light", 8F);
-            this.label10.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label10.ForeColor = System.Drawing.Color.Chocolate;
             this.label10.Location = new System.Drawing.Point(483, 162);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(61, 18);
@@ -452,7 +452,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Mitr Light", 8F);
-            this.label7.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label7.ForeColor = System.Drawing.Color.Chocolate;
             this.label7.Location = new System.Drawing.Point(142, 241);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(34, 18);
@@ -489,7 +489,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Mitr Light", 8F);
-            this.label5.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label5.ForeColor = System.Drawing.Color.Chocolate;
             this.label5.Location = new System.Drawing.Point(136, 282);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(40, 18);
@@ -649,14 +649,14 @@
             this.GridView.BackgroundColor = System.Drawing.Color.WhiteSmoke;
             this.GridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.GridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Mitr Light", 9.75F);
-            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Gray;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Silver;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.GridView.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Mitr Light", 9.75F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Gray;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Silver;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.GridView.DefaultCellStyle = dataGridViewCellStyle2;
             this.GridView.GridColor = System.Drawing.Color.Silver;
             this.GridView.Location = new System.Drawing.Point(19, 60);
             this.GridView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
@@ -919,7 +919,7 @@
             // 
             this.label32.AutoSize = true;
             this.label32.Font = new System.Drawing.Font("Mitr Light", 8F);
-            this.label32.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label32.ForeColor = System.Drawing.Color.Chocolate;
             this.label32.Location = new System.Drawing.Point(134, 201);
             this.label32.Name = "label32";
             this.label32.Size = new System.Drawing.Size(42, 18);
@@ -941,7 +941,7 @@
             // 
             this.label28.AutoSize = true;
             this.label28.Font = new System.Drawing.Font("Mitr Light", 8F);
-            this.label28.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label28.ForeColor = System.Drawing.Color.Chocolate;
             this.label28.Location = new System.Drawing.Point(861, 241);
             this.label28.Name = "label28";
             this.label28.Size = new System.Drawing.Size(51, 18);
@@ -963,7 +963,7 @@
             // 
             this.label26.AutoSize = true;
             this.label26.Font = new System.Drawing.Font("Mitr Light", 8F);
-            this.label26.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label26.ForeColor = System.Drawing.Color.Chocolate;
             this.label26.Location = new System.Drawing.Point(863, 162);
             this.label26.Name = "label26";
             this.label26.Size = new System.Drawing.Size(49, 18);
@@ -985,7 +985,7 @@
             // 
             this.label18.AutoSize = true;
             this.label18.Font = new System.Drawing.Font("Mitr Light", 8F);
-            this.label18.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label18.ForeColor = System.Drawing.Color.Chocolate;
             this.label18.Location = new System.Drawing.Point(147, 162);
             this.label18.Name = "label18";
             this.label18.Size = new System.Drawing.Size(29, 18);
@@ -1025,7 +1025,6 @@
             this.txtGoldPriceSell.Size = new System.Drawing.Size(113, 26);
             this.txtGoldPriceSell.TabIndex = 1;
             this.txtGoldPriceSell.TabStop = false;
-            this.txtGoldPriceSell.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtGoldPriceSell_KeyPress);
             this.txtGoldPriceSell.Leave += new System.EventHandler(this.txtGoldPriceSell_Leave);
             // 
             // cbbStatus
